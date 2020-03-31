10ff7266b8ecde3775a5b22d142024d1990fc113
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-03-31 16:35:23 +0300

    Lomakkeen aloitus

--- src/main/java/project/main/Main.java
+++ src/main/java/project/main/Main.java
@@ -1,10 +1,24 @@
 package project.main;
 
+import spark.ModelAndView;
+import spark.Spark;
+import static spark.Spark.port;
+
+import java.util.HashMap;
+
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        
+        port(4569);
+        Spark.get("/", (req, res) -> {
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "lomake");
+        }, new ThymeleafTemplateEngine());
     }
     
 }
